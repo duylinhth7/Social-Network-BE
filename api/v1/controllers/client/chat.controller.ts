292aaddc0d51a054addc3fac4fe17e25a624eba7
duylinhth7 <hoanglinhdz2k4@gmail.com>
2025-06-01 23:10:04 +0700

    update

--- api/v1/controllers/client/chat.controller.ts
+++ api/v1/controllers/client/chat.controller.ts
@@ -1,57 +1,25 @@
 import { Request, Response } from "express";
 import Chat from "../../models/client/chat.model";
+import User from "../../models/client/user.model";
 
 // [GET] /index
-export const index = async (req: Request, res: Response): Promise<void> => {
+export const index = async (req: Request, res: Response): Promise<void> =>  {
   try {
     const roomChatId = req.params.id;
-
-    const chats = await Chat.aggregate([
-      {
-        $match: {
-          deleted: false,
-          room_chat_id: roomChatId,
-        },
-      },
-      { $sort: { updatedAt: 1 } },
-      {
-        $lookup: {
-          from: "users", // tên collection MongoDB
-          localField: "user_id",
-          foreignField: "_id",
-          as: "infoUser",
-        },
-      },
-      { $unwind: "$infoUser" },
-      {
-        $project: {
-          message: 1,
-          user_id: 1,
-          room_chat_id: 1,
-          updatedAt: 1,
-          deleted: 1,
-          "infoUser.fullName": 1,
-          "infoUser.avatar": 1,
-        },
-      },
-    ]);
-    //
-    if (chats.length > 0) {
+    const chats = await Chat.find({
+      deleted: false,
+      room_chat_id: roomChatId,
+    });
+    if(chats){
       res.json({
         code: 200,
-        chats: chats,
-      });
-    } else {
-      res.json({
-        code: 400,
-        message: "Không có đoạn chat nào!",
-      });
+        chats: chats
+      })
     }
   } catch (error) {
-    console.error(error);
     res.json({
       code: 400,
-      message: "Lỗi",
-    });
+      message: "Lỗi!"
+    })
   }
-};
+}
--- dist/api/v1/controllers/client/chat.controller.js
+++ dist/api/v1/controllers/client/chat.controller.js
@@ -17,53 +17,21 @@ const chat_model_1 = __importDefault(require("../../models/client/chat.model"));
 const index = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const roomChatId = req.params.id;
-        const chats = yield chat_model_1.default.aggregate([
-            {
-                $match: {
-                    deleted: false,
-                    room_chat_id: roomChatId,
-                },
-            },
-            { $sort: { updatedAt: 1 } },
-            {
-                $lookup: {
-                    from: "users",
-                    localField: "user_id",
-                    foreignField: "_id",
-                    as: "infoUser",
-                },
-            },
-            { $unwind: "$infoUser" },
-            {
-                $project: {
-                    message: 1,
-                    user_id: 1,
-                    room_chat_id: 1,
-                    updatedAt: 1,
-                    deleted: 1,
-                    "infoUser.fullName": 1,
-                    "infoUser.avatar": 1,
-                },
-            },
-        ]);
-        if (chats.length > 0) {
+        const chats = yield chat_model_1.default.find({
+            deleted: false,
+            room_chat_id: roomChatId,
+        });
+        if (chats) {
             res.json({
                 code: 200,
-                chats: chats,
-            });
-        }
-        else {
-            res.json({
-                code: 400,
-                message: "Không có đoạn chat nào!",
+                chats: chats
             });
         }
     }
     catch (error) {
-        console.error(error);
         res.json({
             code: 400,
-            message: "Lỗi",
+            message: "Lỗi!"
         });
     }
 });
